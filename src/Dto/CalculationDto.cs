2854c593f237df4e2e1fda6a6753836df08c1dcc
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-09 13:40:42 -0300

    feat: adicionado consumo na api

--- src/Dto/CalculationDto.cs
+++ src/Dto/CalculationDto.cs
@@ -1,12 +1,25 @@
-﻿namespace CalculadoraTeste.Dto
+﻿using System.Text.Json.Serialization;
+
+namespace CalculadoraTeste.Dto
 {
     public class CalculationDto
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+
+        [JsonPropertyName("valorA")]
         public int ValueA { get; set; }
+
+        [JsonPropertyName("valorB")]
         public int ValueB { get; set; }
+
+        [JsonPropertyName("operacao")]
         public string Operation { get; set; } = string.Empty;
+
+        [JsonPropertyName("resultado")]
         public int Result { get; set; }
+
+        [JsonPropertyName("dataCalculo")]
         public DateTime CreatedAt { get; set; }
     }
 }
