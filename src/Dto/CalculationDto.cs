7b82c8490e959bf4f2f0d550e9511b552024e213
nicolasmiaguchiku <nicolaseeisuke@gmail.com>
2026-04-08 22:18:18 -0300

    refactor: dados mocados enquanto a api não esta funcionando

---
+++ src/Dto/CalculationDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CalculadoraTeste.Dto
+{
+    public class CalculationDto
+    {
+        public int Id { get; set; }
+        public int ValueA { get; set; }
+        public int ValueB { get; set; }
+        public string Operation { get; set; } = string.Empty;
+        public int Result { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,10 @@
 using CalculadoraTeste.Components;
+using CalculadoraTeste.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<CalculationStateService>();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
